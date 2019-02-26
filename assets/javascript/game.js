bb9f6a9382d171077bc6a15c16724c0bf3cd5b9c
jbribiesca <bribiesca@gmail.com>
2019-02-25 19:15:54 -0600

    finished game

---
+++ assets/javascript/game.js
@@ -0,0 +1,48 @@
+$(document).ready(function() {
+
+    var randomNumber = 0;
+    var wins = 0;
+    var losses = 0;
+    var totalScore = 0;
+    var crystalBlue = 0;
+    var crystalRed = 0;
+    var crystalPurple = 0;
+    var crystalUnicorn = 0;
+  
+    function initializeGame() {
+        randomNumber = Math.floor(Math.random() * 120) + 10; 
+        crystalBlue = Math.floor(Math.random() * 12) + 1; 
+        crystalRed = Math.floor(Math.random() * 12) + 1; 
+        crystalPurple = Math.floor(Math.random() * 12) + 1; 
+        crystalUnicorn = Math.floor(Math.random() * 12) + 1; 
+        $("#ranNum").text(randomNumber)
+        $("#crystal1").attr("data-crystalvalue", crystalBlue)
+        $("#crystal2").attr("data-crystalvalue", crystalRed)
+        $("#crystal3").attr("data-crystalvalue", crystalPurple)
+        $("#crystal4").attr("data-crystalvalue", crystalUnicorn)
+        totalScore = 0;
+        $("#totScore").empty();
+    }
+    
+    initializeGame();
+
+    $(".crystalImage").on("click", function() {
+        var crystalValue = ($(this).attr("data-crystalvalue"));
+        crystalValue = parseInt(crystalValue);
+        totalScore += crystalValue
+        $("#totScore").text(totalScore)
+
+        if (totalScore === randomNumber) {
+            wins++;
+            $("#wintext").text(wins)
+            initializeGame();
+        }
+        else if (totalScore >= randomNumber) {
+            losses++;
+            $("#losstext").text(losses)
+            initializeGame();
+        }
+    })
+
+
+})
